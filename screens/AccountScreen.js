4a8d9bdd8dd872fd18fa5cd9aecbadd5783a0403
sylvie <sylvieqian@hotmail.com>
2022-06-30 12:30:58 +0800

    Add Loading Indicator

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { commonStyles } from "../styles/commonStyles";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
@@ -19,19 +25,32 @@ export default function AccountScreen({ navigation }) {
         headers: { Authorization: `JWT ${token}` },
       });
       console.log("Got user name!");
-      console.log(response);
+      setUsername(response.data.username);
     } catch (error) {
       console.log("Error getting user name");
       if (error.response) {
         console.log(error.response.data);
+        if (error.response.data.status_code === 401) {
+          signOut();
+        }
       } else {
         console.log(error);
       }
+      // We should probably go back to the login screen???
     }
   }
 
   useEffect(() => {
+    console.log("Setting up nav listener");
+    // Check for when we come back to this screen
+    const removeListener = navigation.addListener("focus", () => {
+      console.log("Running nav listener");
+      setUsername(<ActivityIndicator />);
+      getUsername();
+    });
     getUsername();
+
+    return removeListener;
   }, []);
 
   function signOut() {
